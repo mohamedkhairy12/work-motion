575c7df3f045513572a4c365494733de1ef61831
muhamedkhairy <mohamed.khairy.int@homzmart.com>
2021-11-19 16:40:11 +0200

    card

--- components/searchInput/searchInput.js
+++ components/searchInput/searchInput.js
@@ -2,59 +2,117 @@ import React, { useState, useEffect } from "react";
 import Styles from "./assets/searchInput.module.scss";
 
 const SearchInput = () => {
-  const [show, setShow] = useState(false);
-  const [valueclick, setValueclick] = useState("");
-  const [valueInput, setValueInput] = useState("");
-  const [news, setNews] = useState("");
+  const [showCountry, setShowCountry] = useState(false);
+  const [showJop, setShowJop] = useState(false);
+  const [countryValue, setCountryValue] = useState("");
+  const [jopValue, setJopValue] = useState("");
+  const [getValueCountry, setGetValueCountry] = useState("");
+  const [getValuejop, setGetValuejop] = useState("");
+  const [newCountries, setNewCountries] = useState("");
+  const [newjops, setNewJops] = useState("");
   const posts = [
     { id: "1", name: "This first post is about React" },
     { id: "2", name: "This next post is about Preact" },
     { id: "3", name: "We have yet another React post!" },
     { id: "4", name: "This is the fourth and final post" },
   ];
-  const handleOpen = () => {
-    setShow(true);
+  const posts1 = [
+    { id: "1", name: "This first post is about React" },
+    { id: "2", name: "This next post is about Preact" },
+    { id: "3", name: "We have yet another React post!" },
+    { id: "4", name: "This is the fourth and final post" },
+  ];
+  const handleOpenJops = () => {
+    setShowJop(true);
   };
-  const getData = (e) => {
+  const handleOpencountries = () => {
+    setShowCountry(true);
+  };
+  const getCountriesData = (e) => {
     console.log(e.target.firstChild.data);
-    setValueclick(e.target.firstChild.data);
-    setValueInput("");
-    setShow(false);
+    setCountryValue(e.target.firstChild.data);
+    setGetValueCountry("");
+    setShowCountry(false);
   };
-  const getValue = (e) => {
-    setValueInput(e.target.value);
+  const getJopsData = (e) => {
+    console.log(e.target.firstChild.data);
+    setJopValue(e.target.firstChild.data);
+    setGetValuejop("");
+    setShowJop(false);
+  };
+  const getCountry = (e) => {
+    setGetValueCountry(e.target.value);
+    console.log(e.target.value);
+  };
+  const getjop = (e) => {
+    setGetValuejop(e.target.value);
     console.log(e.target.value);
   };
   useEffect(() => {
-    const results = posts.filter((person) =>
-      person.name.toLowerCase().includes(valueInput)
+    const country = posts.filter((person) =>
+      person.name.toLowerCase().includes(getValueCountry)
     );
-    setNews(results);
-    console.log(results);
-  }, [valueInput]);
+    setNewCountries(country);
+    console.log(country);
+
+    const jop = posts.filter((person) =>
+      person.name.toLowerCase().includes(getValuejop)
+    );
+    setNewJops(jop);
+    console.log(jop);
+  }, [getValueCountry, getValuejop]);
+
+  const sendDaTa = () => {
+    console.log([{ country: countryValue, jop: jopValue }]);
+  };
   return (
     <>
-      <div className="container">
-        <div className={`row ${Styles.container}`}>
-          <div className={`col-6 ${Styles.card}`}>
-            <div className={Styles.searchInput} onInput={handleOpen}>
-              <input
-                type="text"
-                value={valueInput}
-                placeholder={valueclick}
-                onInput={getValue}
-              />
-             
-            </div>
-            {show ? (
+      <div className={`container ${Styles.container}`}>
+        <div className={`row ${Styles.card}`}>
+          <div
+            className={`col-12 col-md-5 ${Styles.searchInput}`}
+            onInput={handleOpenJops}
+          >
+            <input
+              type="text"
+              value={getValuejop}
+              placeholder={jopValue ? jopValue : "Search by job title"}
+              onInput={getjop}
+            />
+            {showJop ? (
               <div className={Styles.cardSearch}>
-                <ul onClick={getData}>
-                  {news.map((post) => (
+                <ul onClick={getJopsData}>
+                  {newjops.map((post) => (
                     <li key={post.id}>{post.name}</li>
                   ))}
                 </ul>
               </div>
             ) : null}
+          </div>
+          <div
+            className={`col-12 col-md-5 ${Styles.searchInput}`}
+            onInput={handleOpencountries}
+          >
+            <input
+              type="text"
+              value={getValueCountry}
+              placeholder={
+                countryValue ? countryValue : "Search by city, or country"
+              }
+              onInput={getCountry}
+            />
+            {showCountry ? (
+              <div className={Styles.cardSearch}>
+                <ul onClick={getCountriesData}>
+                  {newCountries.map((post) => (
+                    <li key={post.id}>{post.name}</li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+          <div className={`col-2 col-md-2 ${Styles.btn}`} onClick={sendDaTa}>
+            <button>Search</button>
           </div>
         </div>
       </div>
