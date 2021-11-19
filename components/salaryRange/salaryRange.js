795ce8b3a0df374660a08ad445d9f0a8f2762c99
gehadahmed2 <gehad.ahmed@homzmart.com>
2021-11-19 16:19:48 +0200

    footer and ranges feature

--- components/salaryRange/salaryRange.js
+++ components/salaryRange/salaryRange.js
@@ -2,119 +2,24 @@ import React, { useState } from 'react'
 // import ReactRanger from 'react-ranger'
 // import { useRanger } from 'react-ranger'
 import ReactDOM from "react-dom";
-import styled, { createGlobalStyle } from "styled-components";
+// import styled, { createGlobalStyle } from "styled-components";
 import { useRanger } from "react-ranger";
 import Styles from './assets/salaryRange.module.scss'
-const GlobalStyles = createGlobalStyle`
-body {
- font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
- font-weight: 300;
-}
-`;
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-export const Track = styled("div")`
-display: inline-block;
-height: 8px;
-width: 90%;
-margin: 0 5%;
-`;
-
-export const Tick = styled("div")`
-:before {
-  content: "";
-  position: absolute;
-  left: 0;
-  background: rgba(0, 0, 0, 0.2);
-  height: 5px;
-  width: 2px;
-  transform: translate(-50%, 0.7rem);
-}
-`;
-
-export const TickLabel = styled("div")`
-position: absolute;
-font-size: 0.6rem;
-color: rgba(0, 0, 0, 0.5);
-top: 100%;
-transform: translate(-50%, 1.2rem);
-white-space: nowrap;
-`;
-
-export const Segment = styled("div")`
-background: ${props =>
-        props.index === 0
-            ? "#3e8aff"
-            : props.index === 1
-                ? "#00d5c0"
-                : props.index === 2
-                    ? "#f5c200"
-                    : "#ff6050"};
-height: 100%;
-`;
-
-export const Handle = styled("div")`
-background: #ff1a6b;
-display: flex;
-align-items: center;
-justify-content: center;
-width: 1.6rem;
-height: 1.6rem;
-border-radius: 100%;
-font-size: 0.7rem;
-white-space: nowrap;
-color: white;
-font-weight: ${props => (props.active ? "bold" : "normal")};
-transform: ${props =>
-        props.active ? "translateY(-100%) scale(1.3)" : "translateY(0) scale(0.9)"};
-transition: all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-`;
 const salaryRange = () => {
-
-    const [values, setValues] = React.useState([15, 50, 80]);
-
-    const { getTrackProps, ticks, segments, handles } = useRanger({
-        min: 0,
-        max: 100,
-        stepSize: 1,
-        values,
-        onChange: setValues
-    });
+const [radio, setRadio]= useState('apple')
     return (
-
         <div className="container">
             <div className={Styles.ranges}>
+                <p className={Styles.title}>Check Salary Ranges</p>
+                <hr style={{border: "1px solid #f0eeee"}}/>
+                <label>{radio}</label>
+                <input type="radio" value="apple" checked={radio === "apple" } onChange={(e)=>{setRadio(e.target.value)}} />
+                <input type="radio" value="orange" checked={radio === "orange"} onChange={(e)=>{setRadio(e.target.value)}} />
                 <div className="row">
-                    <p>Graphic Designer Salary in Egypt</p>
-
-                    <GlobalStyles />
-                    <h1>Custom Styles</h1>
-                    <br />
-                    <br />
-                    <Track {...getTrackProps()}>
-                        {ticks.map(({ value, getTickProps }) => (
-                            <Tick {...getTickProps()}>
-                                <TickLabel>{value}</TickLabel>
-                            </Tick>
-                        ))}
-                        {segments.map(({ getSegmentProps }, i) => (
-                            <Segment {...getSegmentProps()} index={i} />
-                        ))}
-                        {handles.map(({ value, active, getHandleProps }) => (
-                            <button
-                                {...getHandleProps({
-                                    style: {
-                                        appearance: "none",
-                                        border: "none",
-                                        background: "transparent",
-                                        outline: "none"
-                                    }
-                                })}
-                            >
-                                <Handle active={active}>{value}</Handle>
-                            </button>
-                        ))}
-                    </Track>
-                   
+                {/* <Range max={60} min={10} step={8}/> */}
                 </div>
             </div>
         </div>
