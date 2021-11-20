442d2280031c3dde36a474c13242af7646d50cd9
gehadahmed2 <gehad.ahmed@homzmart.com>
2021-11-20 16:29:39 +0200

    resposnsive

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -19,16 +19,16 @@ const footer = () => {
                         </div>
                         <p className={Styles.address}>Richard-Ermisch-Str. 7, 10247 Berlin, Germany  WorkMotion Software GmbH registered with the commercial register in Germany under HRB 219211 B.</p>
                     </div>
-                    <div className="col-6 col-sm-2">
+                    <div className="col-12 col-sm-2">
                         <p className={Styles.title}>Product</p>
                         <p className={Styles.desc}>Workmotion Platform</p>
                     </div>
-                    <div className="col-6 col-sm-2">
+                    <div className="col-12 col-sm-2">
                         <p className={Styles.title}>Services</p>
                         <p className={Styles.desc}>How it works</p>
                         <p className={Styles.desc}>Features</p>
                     </div>
-                    <div className="col-6 col-sm-2">
+                    <div className="col-12 col-sm-2">
                         <p className={Styles.title}>Resources</p>
                         <p className={Styles.desc}>Blog</p>
                         <p className={Styles.desc}>FAQ</p>
@@ -37,11 +37,11 @@ const footer = () => {
                         <p className={Styles.desc}>Help Center</p>
 
                     </div>
-                    <div className="col-6 col-sm-2">
+                    <div className="col-12 col-sm-2">
                         <p className={Styles.title}>About Us</p>
                         <p className={Styles.desc}>Meet our team</p>
                     </div>
-                    <div className="col-6 col-sm-2">
+                    <div className="col-12 col-sm-2">
                         <p className={Styles.title}>Careers</p>
                         <p className={Styles.desc}>We're hiring</p>
                     </div>
