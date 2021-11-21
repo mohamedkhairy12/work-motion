4bf25922d5af323225b74e2709062e2ae7ff5833
muhamedkhairy <mohamed.khairy.int@homzmart.com>
2021-11-21 18:15:22 +0200

    change image res

--- components/header/header.js
+++ components/header/header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Styles from "./assets/header.module.scss";
 import HeaderPhoto from "./assets/image/header.png";
+import HeaderRes from "./assets/image/headerRes.png";
 
 
 const header = () => {
   return (
     <div className={Styles.container}>
         <img className={Styles.img} src={HeaderPhoto.src}/>
+        <img className={Styles.imgRes} src={HeaderRes.src}/>
     </div>
   );
 };
