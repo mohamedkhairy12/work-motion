28dc67ec090f7a4ad7f1e435419aea47e6c558a6
gehadahmed2 <gehad.ahmed@homzmart.com>
2021-11-21 18:02:44 +0200

    fix styles

--- components/salaryRange/salaryRange.js
+++ components/salaryRange/salaryRange.js
@@ -3,7 +3,7 @@ import Styles from './assets/salaryRange.module.scss'
 
 
 const salaryRange = () => {
-    const [radio, setRadio] = useState('annual');  
+    const [radio, setRadio] = useState('Annual');  
     return (
         <div className="container">
             <div className={Styles.ranges} >
@@ -15,13 +15,13 @@ const salaryRange = () => {
                     <div className="row">
                         <div className={`col-12 col-sm-8 ${Styles.sort1}`}>
                             <div className={Styles.radios}>
-                                <input type="radio" value="annual"
-                                    checked={radio === "annual" ? true : false} onChange={(e) => { setRadio(e.target.value) }}
+                                <input type="radio" value="Annual"
+                                    checked={radio === "Annual" ? true : false} onChange={(e) => { setRadio(e.target.value) }}
                                      />
-                                <label style={{ marginRight: "10px" }} className={radio === "annual" ? Styles.unactive :Styles.active   }>Annual</label>
+                                <label style={{ marginRight: "10px" }} className={radio === "Annual" ? Styles.unactive :Styles.active   }>Annual</label>
 
-                                <input type="radio" value="monthly" checked={radio === "monthly" ? true : false} onChange={(e) => { setRadio(e.target.value) }} />
-                                <label style={{ marginRight: "10px" }} className={radio === "monthly" ? Styles.unactive :Styles.active   }>Monthly</label>
+                                <input type="radio" value="Monthly" checked={radio === "Monthly" ? true : false} onChange={(e) => { setRadio(e.target.value) }} />
+                                <label style={{ marginRight: "10px" }} className={radio === "Monthly" ? Styles.unactive :Styles.active   }>Monthly</label>
                             </div>
                             <div className={Styles.rangeInfo}>
                                 <span className="col-3" style={{ background: "#F8936D", width: "93px", height: "52px" }}></span>
@@ -44,7 +44,7 @@ const salaryRange = () => {
                             </div>
                         </div>
                         <div className={`col-12 col-sm-4 ${Styles.baseSalary}`}>
-                            <p className={Styles.month}>Monthly</p>
+                            <p className={Styles.month}>{radio}</p>
                             <p className={Styles.net}>6.830 EGP</p>
                             <p className={Styles.avg}>Avrage base salary</p>
                         </div>
