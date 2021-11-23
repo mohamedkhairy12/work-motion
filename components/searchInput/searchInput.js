37924d2a4681c042d893684c0d51422f31bf114b
muhamedkhairy <mohamed.khairy.int@homzmart.com>
2021-11-23 23:21:46 +0200

    countries

--- components/searchInput/searchInput.js
+++ components/searchInput/searchInput.js
@@ -6,28 +6,11 @@ import axios from "axios";
 
 const SearchInput = () => {
   const [showCountry, setShowCountry] = useState(false);
-  const [showJop, setShowJop] = useState(false);
   const [countryValue, setCountryValue] = useState("");
-  const [jopValue, setJopValue] = useState("");
   const [getValueCountry, setGetValueCountry] = useState("");
-  const [getValuejop, setGetValuejop] = useState("");
   const [newCountries, setNewCountries] = useState("");
-  const [newjops, setNewJops] = useState("");
-  const posts = [
-    { id: "1", name: "This first post is about React" },
-    { id: "2", name: "This next post is about Preact" },
-    { id: "3", name: "We have yet another React post!" },
-    { id: "4", name: "This is the fourth and final post" },
-  ];
-  const posts1 = [
-    { id: "1", name: "This first post is about React" },
-    { id: "2", name: "This next post is about Preact" },
-    { id: "3", name: "We have yet another React post!" },
-    { id: "4", name: "This is the fourth and final post" },
-  ];
-  const handleOpenJops = () => {
-    setShowJop(true);
-  };
+  const [api, setApi] = useState([]);
+
   const handleOpencountries = () => {
     setShowCountry(true);
   };
@@ -37,69 +20,35 @@ const SearchInput = () => {
     setGetValueCountry("");
     setShowCountry(false);
   };
-  const getJopsData = (e) => {
-    console.log(e.target.firstChild.data);
-    setJopValue(e.target.firstChild.data);
-    setGetValuejop("");
-    setShowJop(false);
-  };
+
   const getCountry = (e) => {
     setGetValueCountry(e.target.value);
     console.log(e.target.value);
   };
-  const getjop = (e) => {
-    setGetValuejop(e.target.value);
-    console.log(e.target.value);
-  };
-  
   useEffect(() => {
-    const country = posts.filter((person) =>
+    axios.get(`http://35.184.155.34/index.php/countries`).then((response) => {
+      const data = response.data;
+      setApi(response.data.list);
+      console.log(response.data.list);
+      console.log(api);
+    });
+    const country = api.filter((person) =>
       person.name.toLowerCase().includes(getValueCountry)
     );
     setNewCountries(country);
     console.log(country);
-    const jop = posts.filter((person) =>
-      person.name.toLowerCase().includes(getValuejop)
-    );
-    setNewJops(jop);
-    console.log(jop);
-  }, [getValueCountry, getValuejop]);
+  }, [getValueCountry]);
 
   const sendDaTa = () => {
-    console.log([{ country: countryValue, jop: jopValue }]);
+    console.log([{ country: countryValue }]);
   };
-  useEffect(()=>{
-    axios.get(`http://34.159.137.99/countries`)
-    .then((response) => {
-      const data = response.data;
-      console.log(response);
-    });
-  },[])
+
   return (
     <>
       <div className="container">
         <div className={Styles.cont}>
           <div className={Styles.card}>
             <div className="row">
-              <div className="col-12 col-md-5" onInput={handleOpenJops}>
-                <div className={Styles.searchInput}>
-                  <input
-                    type="text"
-                    value={getValuejop}
-                    placeholder={jopValue ? jopValue : "Search by job title"}
-                    onInput={getjop}
-                  />
-                  {showJop ? (
-                    <div className={Styles.cardSearch}>
-                      <ul onClick={getJopsData}>
-                        {newjops.map((post) => (
-                          <li key={post.id}>{post.name}</li>
-                        ))}
-                      </ul>
-                    </div>
-                  ) : null}
-                </div>
-              </div>
               <div className="col-12 col-md-5" onInput={handleOpencountries}>
                 <div className={Styles.searchInput}>
                   <input
@@ -126,8 +75,7 @@ const SearchInput = () => {
                 onClick={sendDaTa}
               >
                 <button>
-           
-                  <span style={{marginRight:"12px"}}>
+                  <span style={{ marginRight: "12px" }}>
                     <img src={Search.src} />
                   </span>
                   Find Salary
