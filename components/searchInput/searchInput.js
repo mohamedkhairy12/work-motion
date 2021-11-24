5e1761968f6f6736bb88ba1e4b1a33b51df409e5
muhamedkhairy <mohamed.khairy.int@homzmart.com>
2021-11-24 18:57:22 +0200

    last commit

--- components/searchInput/searchInput.js
+++ components/searchInput/searchInput.js
@@ -7,76 +7,84 @@ import axios from "axios";
 
 const SearchInput = () => {
   const [showJop, setShowJop] = useState(false);
-  const [jopValue, setJopValue] = useState("");
+  // const [jopValue, setJopValue] = useState("");
   const [getValuejop, setGetValuejop] = useState("");
   const [newjops, setNewJops] = useState("");
-  const [apiJops, setApiJops] = useState([]);
   const [getRanges, setGetRanges] = useState("");
+
+  // const onFocus = () => setFocused(true);
+  const onBlur = () => {
+      // setFocused(false);
+      setNewJops([])
+ 
+  };
+
   ////////////////////countries/////////////////////
   const [showCountry, setShowCountry] = useState(false);
   const [countryValue, setCountryValue] = useState("");
   const [getValueCountry, setGetValueCountry] = useState("");
   const [newCountries, setNewCountries] = useState("");
   const [api, setApi] = useState([]);
-  const [countryID, setCountryID] = useState('')
-  const [JobID, setJobID] = useState('')
+  const [countryID, setCountryID] = useState("");
+  const [JobID, setJobID] = useState("");
   const handleOpenJops = () => {
     setShowJop(true);
   };
 
   const getJopsData = (e) => {
-    console.log(e.target.firstChild.data);
-    setJopValue(e.target.firstChild.data);
-    setGetValuejop("");
-    setShowJop(false);
-  };
-  const getjop = (e) => {
-    setGetValuejop(e.target.value);
-    console.log(e.target.value);
+    console.log(e);
+    // setJopValue(e.target.firstChild.data);
+    setGetValuejop(e.target.firstChild.data);
+    // setShowJop(false);
+    
+    onBlur();
   };
   const catchJopids = (post) => {
     console.log(post, "ssssss");
-    setJobID(post)
+    setJobID(post);
   };
   useEffect(() => {
+    console.log("here will hit ");
+    
     axios
       .get(`http://35.184.155.34/index.php/category_positions`)
       .then((response) => {
-        const data = response.data.list;
-        setApiJops(response.data.list);
-        console.log(response.data.list);
+        setNewJops(
+          response.data.list.filter((person) =>
+            person.positionName.toLowerCase().includes(getValuejop)
+          )
+        );
       });
-    const jop = apiJops.filter((person) =>
-      person.positionName.toLowerCase().includes(getValuejop)
-    );
-    setNewJops(jop);
-    console.log(jop, "SSSSSSSSSSSSSS");
   }, [getValuejop]);
 
+  const onChangeValueJob = (e) => {
+
+    setGetValuejop(e.target.value);
+    setNewJops([])
+    console.log(e.target.value);
+  };
   const handleOpencountries = () => {
     setShowCountry(true);
   };
   const getCountriesData = (e) => {
-
     console.log(e.currentTarget.firstChild.data, "ssssss");
 
     setCountryValue(e.target.firstChild.data);
-    setGetValueCountry("");
+    // setGetValueCountry("");
+    // getValueCountry
+    setGetValueCountry(e.target.firstChild.data);
     setShowCountry(false);
   };
   const catchCountryids = (post) => {
     console.log(post, "ssssss");
-    setCountryID(post)
+    setCountryID(post);
   };
-
-
 
   const getCountry = (e) => {
     setGetValueCountry(e.target.value);
     console.log(e.target.value);
   };
   useEffect(() => {
-
     axios.get(`http://35.184.155.34/index.php/countries`).then((response) => {
       const data = response.data;
       setApi(response.data.list);
@@ -93,16 +101,17 @@ const SearchInput = () => {
   const sendDaTa = async () => {
     try {
       await axios
-        .get(`http://35.184.155.34/index.php/country/${countryID}/position/${JobID}/advanced`)
+        .get(
+          `http://35.184.155.34/index.php/country/${countryID}/position/${JobID}/advanced`
+        )
         .then((response) => {
-          setGetRanges(response.data)
-          console.log(response.data.currency, "ressssssssss")
+          setGetRanges(response.data);
+          console.log(response.data.currency, "ressssssssss");
         });
     } catch (e) {
-      document.getElementById("red").style.borderColor = "red"
-      console.log(e)
+      document.getElementById("red").style.borderColor = "red";
+      console.log(e);
     }
-
   };
 
   return (
@@ -118,35 +127,47 @@ const SearchInput = () => {
                     type="text"
                     value={getValuejop}
                     placeholder="Search by job title"
-                    onInput={getjop}
+                    onInput={onChangeValueJob}
+                    // onFocus={onFocus}
+                    onBlur={onBlur}
                   />
-                  {showJop ? (
-                    <div className={Styles.cardSearch}>
+                  
+                    {newjops&&newjops.length>0&&<div className={Styles.cardSearch}>
                       <ul onClick={getJopsData}>
-                        {newjops.map((post) => (
-                          <li onClick={() => catchJopids(post.id)} key={post.id}>{post.positionName}</li>
-                        ))}
+                        {newjops &&
+                          newjops?.map((post) => (
+                            <li
+                              onClick={() => catchJopids(post.id)}
+                              key={post.id}
+                            >
+                              {post.positionName}
+                            </li>
+                          ))}
                       </ul>
-                    </div>
-                  ) : null}
+                    </div>}
+                
                 </div>
               </div>
+
               <div className="col-12 col-md-5" onInput={handleOpencountries}>
                 <div className={Styles.searchInput}>
                   <input
                     id="red"
                     type="text"
                     value={getValueCountry}
-                    placeholder={
-                      countryValue ? countryValue : "Search by city, or country"
-                    }
+                    placeholder={"Search by city, or country"}
                     onInput={getCountry}
                   />
                   {showCountry ? (
                     <div className={Styles.cardSearch}>
                       <ul onClick={getCountriesData}>
                         {newCountries.map((post) => (
-                          <li onClick={() => catchCountryids(post.id)} key={post.id}>{post.name}</li>
+                          <li
+                            onClick={() => catchCountryids(post.id)}
+                            key={post.id}
+                          >
+                            {post.name}
+                          </li>
                         ))}
                       </ul>
                     </div>
